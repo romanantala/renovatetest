9c1f34377c3dda7d05c43d37d8b103296c4e2248
Roman Antala <roman.antala_ges@omio.com>
2024-03-18 19:41:31 +0100

    init spring bot

---
+++ src/test/java/com/renovate/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renovate.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
